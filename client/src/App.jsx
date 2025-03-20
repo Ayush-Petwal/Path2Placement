ae8ffd1f9eaeef6c9004e8ab961c8c1c667fc47c
Ayush-Petwal <Ayushpetwal003@gmail.com>
2025-03-20 13:21:43 +0530

    start PPP

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className=" flex flex-col items-center justify-center h-screen">
+      <Button>
+        PlacementPathPilot
+      </Button>
+    </div>
+  )
+}
+
+export default App
